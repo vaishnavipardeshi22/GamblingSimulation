0c16249e63109f33c52ee83724761d14c8f8b008
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-19 22:09:39 +0530

    ADDED : Checking Feature whether Gambler Wins Or Loose the Bet

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -6,3 +6,20 @@ echo " ****************************** WELCOME TO GAMBLING SIMULATION ***********
 
 STAKE_PER_DAY=100
 BET_PER_GAME=1
+WIN=1
+
+#VARIABLES
+stake=$STAKE_PER_DAY
+
+#GET RANDOM VALUE FOR GAMBLING
+gamblerCheck=$(( RANDOM % 2 ))
+
+#CHECKING GAMBLER WIN OR LOOSE THE BET AND CALCULATE THE STAKE
+if [ $gamblerCheck -eq $WIN ]
+then
+	echo "Gambler wins the bet by \$$BET_PER_GAME."
+	stake=$(( stake + BET_PER_GAME ))
+else
+	echo "Gambler loose the bet by \$$BET_PER_GAME."
+	stake=$(( stake - BET_PER_GAME))
+fi
