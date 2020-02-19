73dd90a41f53d0b6dd73d30443bd626d2d7346c3
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-19 22:27:57 +0530

    ADDED : Resigning Condition For Win And Loose

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -7,19 +7,21 @@ echo " ****************************** WELCOME TO GAMBLING SIMULATION ***********
 STAKE_PER_DAY=100
 BET_PER_GAME=1
 WIN=1
+MIN_WIN=$(( STAKE_PER_DAY * 50 / 100 ))
+MAX_WIN=$(( STAKE_PER_DAY + MIN_WIN ))
 
-#VARIABLES
+#VARIABLE
 stake=$STAKE_PER_DAY
 
-#GET RANDOM VALUE FOR GAMBLING
-gamblerCheck=$(( RANDOM % 2 ))
-
 #CHECKING GAMBLER WIN OR LOOSE THE BET AND CALCULATE THE STAKE
-if [ $gamblerCheck -eq $WIN ]
-then
-	echo "Gambler wins the bet by \$$BET_PER_GAME."
-	stake=$(( stake + BET_PER_GAME ))
-else
-	echo "Gambler loose the bet by \$$BET_PER_GAME."
-	stake=$(( stake - BET_PER_GAME))
-fi
+while [ $stake -gt $MIN_WIN -a $stake -lt $MAX_WIN ]
+do
+	gamblerCheck=$(( RANDOM % 2 ))
+
+	if [ $gamblerCheck -eq $WIN ]
+	then
+		stake=$(( stake + BET_PER_GAME ))
+	else
+		stake=$(( stake - BET_PER_GAME))
+	fi
+done
