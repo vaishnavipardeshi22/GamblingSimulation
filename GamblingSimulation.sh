6d63f256796b4627edb3bd5ca5a1bc6c84cd288a
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-20 10:02:23 +0530

    ADDED : Calculate Total Amount of Win or Lose For 20 Days

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -9,19 +9,37 @@ BET_PER_GAME=1
 WIN=1
 MIN_WIN=$(( STAKE_PER_DAY * 50 / 100 ))
 MAX_WIN=$(( STAKE_PER_DAY + MIN_WIN ))
+TOTAL_DAYS=20
 
-#VARIABLE
-stake=$STAKE_PER_DAY
+#FUNCTION FOR CALCULATING STAKE PER DAY
+function calculateDailyStake()
+{
+	local stake=$STAKE_PER_DAY
 
-#CHECKING GAMBLER WIN OR LOOSE THE BET AND CALCULATE THE STAKE
-while [ $stake -gt $MIN_WIN -a $stake -lt $MAX_WIN ]
+	while [ $stake -gt $MIN_WIN -a $stake -lt $MAX_WIN ]
+	do
+		gamblerCheck=$(( RANDOM % 2 ))
+
+		if [ $gamblerCheck -eq $WIN ]
+		then
+			stake=$(( stake + BET_PER_GAME ))
+		else
+			stake=$(( stake - BET_PER_GAME))
+		fi
+	done
+	stake=$(( stake - STAKE_PER_DAY ))
+	echo $stake
+}
+
+#CALCULATING STAKE FOR 20 DAYS
+for(( days=1; days<=$TOTAL_DAYS; days++ ))
 do
-	gamblerCheck=$(( RANDOM % 2 ))
-
-	if [ $gamblerCheck -eq $WIN ]
-	then
-		stake=$(( stake + BET_PER_GAME ))
-	else
-		stake=$(( stake - BET_PER_GAME))
-	fi
+	totalStake=$(( totalStake + $(calculateDailyStake) ))
 done
+
+if [ $totalStake -ge 0 ]
+then
+	totalStakeWon=$totalStake
+else 
+	totalStakeLost=$((-1*$totalStake))
+fi
